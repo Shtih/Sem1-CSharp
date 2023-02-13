de1fe6c57c5a84f5f67bd59927fd4a2ee69e9986
Shtih <124462344+Shtih@users.noreply.github.com>
2023-02-13 08:23:12 +0300

    add home work

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдает, какое число большее, а какое меньшее.
+//a = 5; b = 7 ->  max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if(A>B)
+{
+    Console.Write($"Большее число {A} Меньшее число {B}");
+}
+else
+{
+    Console.Write($"Большее число {A} Меньшее число {B}");
+}
---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход 
+//три числа и выдает максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число С: ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if(A>B && A>C)
+{
+    Console.Write($"{A} является максимальным");
+}
+else if(B>A && B>C)
+{
+    Console.Write($"{B} является максимальным");
+}
+else if(C>B && C>A)
+{
+    Console.Write($"{C} является максимальным");
+}
